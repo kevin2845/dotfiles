7287452c44ced6827b505bb8b503d51316937b73
kevin2845 <61488413+kevin2845@users.noreply.github.com>
2025-08-17 13:25:52 -0600

    :q

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -741,18 +741,6 @@ require("lazy").setup({
 	--
 	--
 	--
-	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
 
 	{
 		"xvzc/chezmoi.nvim",
