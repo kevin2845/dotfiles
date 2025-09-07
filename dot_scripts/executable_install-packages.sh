b827f6ad248e0da6419ce19dd87ae38725a91518
Kevin <kevin@kevinlynch.dev>
2025-09-07 16:03:11 -0600

    added flatpack and added other distros, redid summary

--- dot_scripts/executable_install-packages.sh
+++ dot_scripts/executable_install-packages.sh
@@ -8,3 +8,9 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 
 ANSIBLE_DISPLAY_SKIPPED_HOSTS=false ansible-playbook "$SCRIPT_DIR/playbooks/install_common_packages.yml" -K 
+
+ANSIBLE_DISPLAY_SKIPPED_HOSTS=false ansible-playbook "$SCRIPT_DIR/playbooks/install_common_packages.yml" -K -v
+
+ANSIBLE_DISPLAY_SKIPPED_HOSTS=false ansible-playbook "$SCRIPT_DIR/playbooks/install_common_packages.yml" -K -vv
+
+ANSIBLE_DISPLAY_SKIPPED_HOSTS=false ansible-playbook "$SCRIPT_DIR/playbooks/install_common_packages.yml" -K -vvv
