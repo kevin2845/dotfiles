95b43d930e39c1eeb703963b24c0c7aaea01b1dd
kevin2845 <61488413+kevin2845@users.noreply.github.com>
2025-09-07 18:23:40 -0600

    fixed sessionizer

--- dot_scripts/executable_tmux-sessionizer.sh
+++ dot_scripts/executable_tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/Developer ~/Desktop ~/ ~/Developer/COMP3421 ~/Developer/SysProgramming ~/.config ~/Developer/Rust-Stuff ~/Repos -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/Developer ~/Desktop ~/ ~/.config ~/Repos ~/.local/share/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
