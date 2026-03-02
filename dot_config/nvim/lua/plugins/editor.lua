9893de4cf529ae791765b1cfed29a361e919b28a
kevin2845 <61488413+kevin2845@users.noreply.github.com>
2026-03-01 20:51:44 -0700

    added proper highlighting for chezmoi files

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -111,17 +111,35 @@ return {
 			},
 			dashboard = {
 				enabled = true,
-                preset = {
-				    keys = {
-                        --- stylua ignore start
-				    	{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-				    	{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-                        { icon = " ", key = "c", desc = "Config", action = function() require("chezmoi.pick").snacks() end },
-                        { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-                        --- stylua ignore end
-				    },
-                },
+				preset = {
+					keys = {
+						--- stylua ignore start
+						{
+							icon = " ",
+							key = "f",
+							desc = "Find File",
+							action = ":lua Snacks.dashboard.pick('files')",
+						},
+						{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+						{
+							icon = " ",
+							key = "c",
+							desc = "Config",
+							action = function()
+								require("chezmoi.pick").snacks()
+							end,
+						},
+						{
+							icon = "󰒲 ",
+							key = "L",
+							desc = "Lazy",
+							action = ":Lazy",
+							enabled = package.loaded.lazy ~= nil,
+						},
+						{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
+						--- stylua ignore end
+					},
+				},
 				sections = {
 					{ section = "header" },
 					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
@@ -155,6 +173,14 @@ return {
 				},
 			},
 		},
+	},
+	-- Chezmoi syntax highlighting
+	{
+		"alker0/chezmoi.vim",
+		init = function()
+			-- This setting is required for proper filetype detection in Neovim
+			vim.g["chezmoi#use_tmp_buffer"] = 1
+		end,
 	},
 
 	-- Chezmoi dotfiles management
